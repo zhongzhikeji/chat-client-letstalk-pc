065f80e108ba911c8fdac041cc195e427a626bd3
crapthings <crapthings@gmail.com>
2023-04-28 14:27:38 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ npm i
 
 npm run build-bypass
 
-tar czf chat-client.tar.gz ./dist
+tar czf chat-client-pc.tar.gz ./dist
 
-scp -o StrictHostKeyChecking=no chat-client.tar.gz root@$IP:$WEBSITE_FOLDER
+scp -o StrictHostKeyChecking=no chat-client-pc.tar.gz root@$IP:/root/server/chat-client-pc
 
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa root@${IP} <<"ENDSSH"
-cd ~/server/chat-client
-tar -xvzf chat-client.tar.gz --strip 1
+cd ~/server/chat-client-pc
+tar -xvzf chat-client-pc.tar.gz --strip 1
 ENDSSH
